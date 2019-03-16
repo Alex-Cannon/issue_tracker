d24359541fd97ab6b453da3b70ba46e2d6839e72
Alex <alexcannon2018@gmail.com>
2019-03-15 20:49:37 -0600

    chore: added a basic api file.

---
+++ api/issues.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+// POST project {issue_title, issue_text, created_by, assigned_to, status_text}
+router.post('/issues/:project_name', (req, res) => {
+  // add {created_on, updated_on, open, and _id}
+  if (!projectExists()) {
+
+  }
+});
+
+// EDIT project given { _id }
+router.put('/issues/:project_name', (req, res) => {
+  // Update {updated_on}
+});
+
+// DELETE project given { _id }
+router.delete('/issues/:project_name', (req, res) => {
+
+});
+
+// GET project given { project_name }
+// FILTER via query "?field=value&&"
+router.get('/issues/:project_name', (req, res) => {
+
+});
+
+// Returns TRUE if a given { project_name } exists
+const projectExists = (projectName) => {
+
+};
+
+module.exports = router;
