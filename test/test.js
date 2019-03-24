81700e3d0b95ef97f1a4c3afb3d8967a8b1031f0
Alex <alexcannon2018@gmail.com>
2019-03-24 11:06:56 -0600

    chore: added after() to close db connection in tests

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { describe, it, afterEach } = require('mocha');
+const { describe, it, after } = require('mocha');
 var mongoose = require('mongoose');
 var assert = require('chai').assert;
 var utils = require('../api/utils.js');
@@ -10,9 +10,6 @@ const routes = require('../api/api.js');
 const axios = require('axios');
 
 const PORT = process.env.PORT || 80;
-const base = 'http://localhost:' + PORT + '/api';
-
-// Connection Test
 mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
 const db = mongoose.connection;
 
@@ -25,48 +22,50 @@ describe('Connection OK?', function () {
   });
 });
 
-db.once('open', function () {
-  describe('projectExists()', function () {
-    it('projectExists("test_project") Should return an Object.', function (done) {
-      utils.projectExists('test_project', function (err, doc) {
-        if (err) { return assert.fail('Internal Server Error.'); }
-        assert.isNotNull(doc);
-        done();
-      });
+describe('projectExists()', function () {
+  it('projectExists("test_project") Should return an Object.', function (done) {
+    utils.projectExists('test_project', function (err, doc) {
+      if (err) { return assert.fail('Internal Server Error.'); }
+      assert.isNotNull(doc);
+      done();
+    });
+  });
+});
+
+describe('requireProjectName()', function () {
+  it('requireProjectName({params: { project_name }}) should return true', function (done) {
+    utils.requireProjectName({ params: { project_name: 'test_project' } }, null, done);
+  });
+
+  it('requireProjectNames({ params: {} }) should call res.status().send(); .', function (done) {
+    utils.requireProjectName({ params: {} }, { status: function () { return { send: () => { done(); } }; } }, () => {
+      assert.fail('Expected res.sendStatus(400); Got next();');
+    });
+  });
+});
+
+describe('API Endpoints OK?', function () {
+  app.use(bodyParser.json({ extended: false }));
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use('/api', routes);
+  it('Should Run OK.', function (done) {
+    app.listen(PORT, function () {
+      done();
     });
   });
 
-  describe('requireProjectName()', function () {
-    it('requireProjectName({params: { project_name }}) should return true', function (done) {
-      utils.requireProjectName({ params: { project_name: 'test_project' } }, null, done);
-    });
-
-    it('requireProjectNames({ params: {} }) should call res.status().send(); .', function (done) {
-      utils.requireProjectName({ params: {} }, { status: function () { return { send: () => { done(); } }; } }, () => {
-        assert.fail('Expected res.sendStatus(400); Got next();');
+  it('GET /api/issues/:project_name should return an Object.', function (done) {
+    axios.get('/api/issues/test_project')
+      .then(function (res) {
+        assert.isObject(res);
+        done();
+      })
+      .catch(function (err) {
+        assert.fail(err.statusMessage);
       });
-    });
   });
 
-  describe('API Endpoints OK?', function () {
-    app.use(bodyParser.json({ extended: false }));
-    app.use(bodyParser.urlencoded({ extended: false }));
-    app.use('/api', routes);
-    it('Should Run OK.', function (done) {
-      app.listen(PORT, function () {
-        done();
-      });
-    });
-
-    it('GET /api/issues/:project_name should return an Object.', function (done) {
-      axios.get('/api/issues/test_project')
-        .then(function (res) {
-          assert.isObject(res);
-          done();
-        })
-        .catch(function (err) {
-          assert.fail(err.statusMessage);
-        });
-    });
+  after(function () {
+    mongoose.connection.close();
   });
 });
